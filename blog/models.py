01cd244bea3bd9ab53f147326f28fee02eefea36
gemo <kim.gemo@gmail.com>
2024-10-28 18:25:27 +0100

    creating blog app and models

---
+++ blog/models.py
@@ -0,0 +1,44 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from django.db import models
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+    # Question auto_now_add ? 
+    # https://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+    )
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="following"
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
+
+    class Meta:
+        # ensures we don't get multiple UserFollows instances
+        # for unique user-user_followed pairs
+        unique_together = (
+            "user",
+            "followed_user",
+        )
