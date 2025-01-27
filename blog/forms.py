23e97fbfc676068467e167650ea39bb8e257eabc
gemo <kim.gemo@gmail.com>
2025-01-27 10:41:02 +0100

    final corrections

--- blog/forms.py
+++ blog/forms.py
@@ -10,7 +10,15 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ["title", "description", "image"]
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk and self.instance.image:
+            # Ajoute un champ HTML pour afficher l'image actuelle
+            self.fields['image'].label = (
+                f'<b>Image*</b> </br> <img src="{self.instance.image.url}" alt="Image actuelle" '
+                f'style="max-width: 200px; max-height: 200px;" />'
+            )
 
 class ReviewForm(forms.ModelForm):
     class Meta:
